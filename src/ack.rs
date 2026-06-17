e02db55171ece6d63345ce6921094812abff3ae9
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2026-06-17 23:46:41 +0530

    (ack) impl of `Completion` {#58}

--- src/ack.rs
+++ src/ack.rs
@@ -9,18 +9,8 @@ use std::{ptr, sync::atomic};
 /// A monotomically increasing epoch used as indentifier for tracking durability of write operations
 pub type EPOCH = u64;
 
-/// Stores [`FrozenError`] related to acknowledgement of durability
-///
-/// ## Example
-///
-/// ```
-/// use frozen_core::ack::AckError;
-///
-/// let error = AckError::default();
-/// assert_eq!(error.get(), None);
-/// ```
 #[derive(Debug)]
-pub struct AckError(atomic::AtomicPtr<FrozenError>);
+struct AckError(atomic::AtomicPtr<FrozenError>);
 
 impl Default for AckError {
     fn default() -> Self {
@@ -37,20 +27,53 @@ impl Drop for AckError {
     }
 }
 
-impl AckError {
+///
+#[derive(Debug)]
+pub struct Completion {
+    current_epoch: atomic::AtomicU64,
+    durable_epoch: atomic::AtomicU64,
+    error: AckError,
+    event: event_listener::Event,
+}
+
+impl Default for Completion {
+    fn default() -> Self {
+        Self {
+            current_epoch: atomic::AtomicU64::new(0),
+            durable_epoch: atomic::AtomicU64::new(0),
+            error: AckError::default(),
+            event: event_listener::Event::new(),
+        }
+    }
+}
+
+impl Completion {
+    ///
+    #[inline]
+    pub fn increment_current_epoch(&self) -> EPOCH {
+        self.current_epoch.fetch_add(1, atomic::Ordering::AcqRel).wrapping_add(1)
+    }
+
+    ///
+    #[inline]
+    pub fn mark_epoch_as_durable(&self, epoch: EPOCH) {
+        self.durable_epoch.store(epoch, atomic::Ordering::Release);
+        self.event.notify(usize::MAX);
+    }
+
     /// Fetch the acknowledgement error (if any)
     ///
     /// ## Example
     ///
     /// ```
-    /// use frozen_core::ack::AckError;
+    /// use frozen_core::ack::Completion;
     ///
-    /// let error = AckError::default();
-    /// assert_eq!(error.get(), None);
+    /// let completion = Completion::default();
+    /// assert_eq!(completion.get_err(), None);
     /// ```
     #[inline]
-    pub fn get(&self) -> Option<FrozenError> {
-        let curr_err = self.0.load(atomic::Ordering::Acquire);
+    pub fn get_err(&self) -> Option<FrozenError> {
+        let curr_err = self.error.0.load(atomic::Ordering::Acquire);
         if hints::unlikely(!curr_err.is_null()) {
             let frozen_error = unsafe { (*curr_err).clone() };
             return Some(frozen_error);
@@ -64,18 +87,18 @@ impl AckError {
     /// ## Example
     ///
     /// ```
-    /// use frozen_core::{ack::AckError, error::{FrozenError, ErrCode}};
+    /// use frozen_core::{ack::Completion, error::{FrozenError, ErrCode}};
     ///
-    /// let ack_error = AckError::default();
+    /// let completion = Completion::default();
     /// let new_error = FrozenError::new(0x10, 0x20, ErrCode::new(0x30, "io"), "failed to read file");
     ///
-    /// ack_error.set(new_error.clone());
-    /// assert_eq!(ack_error.get(), Some(new_error));
+    /// completion.set_err(new_error.clone());
+    /// assert_eq!(completion.get_err(), Some(new_error));
     /// ```
     #[inline]
-    pub fn set(&self, new_error: FrozenError) {
+    pub fn set_err(&self, new_error: FrozenError) {
         let boxed_error = Box::into_raw(Box::new(new_error));
-        let old_err = self.0.swap(boxed_error, atomic::Ordering::AcqRel);
+        let old_err = self.error.0.swap(boxed_error, atomic::Ordering::AcqRel);
 
         if hints::unlikely(!old_err.is_null()) {
             let _ = unsafe { Box::from_raw(old_err) };
@@ -87,22 +110,34 @@ impl AckError {
     /// ## Example
     ///
     /// ```
-    /// use frozen_core::{ack::AckError, error::{FrozenError, ErrCode}};
+    /// use frozen_core::{ack::Completion, error::{FrozenError, ErrCode}};
     ///
-    /// let ack_error = AckError::default();
+    /// let completion = Completion::default();
     /// let new_error = FrozenError::new(0x10, 0x20, ErrCode::new(0x30, "io"), "failed to read file");
     ///
-    /// ack_error.set(new_error.clone());
-    /// assert!(ack_error.get().is_some());
+    /// completion.set_err(new_error.clone());
+    /// assert!(completion.get_err().is_some());
     ///
-    /// ack_error.del();
-    /// assert!(ack_error.get().is_none());
+    /// completion.del_err();
+    /// assert!(completion.get_err().is_none());
     /// ```
     #[inline]
-    pub fn del(&self) {
-        let old_err = self.0.swap(ptr::null_mut(), atomic::Ordering::AcqRel);
+    pub fn del_err(&self) {
+        let old_err = self.error.0.swap(ptr::null_mut(), atomic::Ordering::AcqRel);
         if hints::unlikely(!old_err.is_null()) {
             let _ = unsafe { Box::from_raw(old_err) };
         }
+    }
+
+    ///
+    #[inline]
+    pub fn read_current_epoch(&self) -> EPOCH {
+        self.current_epoch.load(atomic::Ordering::Acquire)
+    }
+
+    ///
+    #[inline]
+    pub fn read_durable_epoch(&self) -> EPOCH {
+        self.durable_epoch.load(atomic::Ordering::Acquire)
     }
 }
