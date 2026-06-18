a90b2d6ed7f0e3cf09c82e7dd1322b9f4f4184fd
Arctic Byte <77727680+adityamotale@users.noreply.github.com>
2026-06-18 10:55:40 +0530

    (ack) impl of `AckTicket` {#58}

--- src/ack.rs
+++ src/ack.rs
@@ -3,11 +3,14 @@
 //! In storage system an acknowledgement is a confirmation that a previously submitted operations
 //! has reached a durable state.
 
-use crate::{error::FrozenError, hints};
-use std::{ptr, sync::atomic};
+use crate::{
+    error::{FrozenError, FrozenResult},
+    hints,
+};
+use std::{future, pin, ptr, sync, sync::atomic, task};
 
 /// A monotomically increasing epoch used as indentifier for tracking durability of write operations
-pub type EPOCH = u64;
+pub type TEpoch = u64;
 
 #[derive(Debug)]
 struct AckError(atomic::AtomicPtr<FrozenError>);
@@ -50,15 +53,14 @@ impl Default for Completion {
 impl Completion {
     ///
     #[inline]
-    pub fn increment_current_epoch(&self) -> EPOCH {
+    pub fn increment_current_epoch(&self) -> TEpoch {
         self.current_epoch.fetch_add(1, atomic::Ordering::AcqRel).wrapping_add(1)
     }
 
     ///
     #[inline]
-    pub fn mark_epoch_as_durable(&self, epoch: EPOCH) {
+    pub fn mark_epoch_as_durable(&self, epoch: TEpoch) {
         self.durable_epoch.store(epoch, atomic::Ordering::Release);
-        self.event.notify(usize::MAX);
     }
 
     /// Fetch the acknowledgement error (if any)
@@ -131,13 +133,97 @@ impl Completion {
 
     ///
     #[inline]
-    pub fn read_current_epoch(&self) -> EPOCH {
+    pub fn read_current_epoch(&self) -> TEpoch {
         self.current_epoch.load(atomic::Ordering::Acquire)
     }
 
     ///
     #[inline]
-    pub fn read_durable_epoch(&self) -> EPOCH {
+    pub fn read_durable_epoch(&self) -> TEpoch {
         self.durable_epoch.load(atomic::Ordering::Acquire)
+    }
+
+    ///
+    #[inline]
+    pub fn notify_all_listeners(&self) {
+        self.event.notify(usize::MAX);
+    }
+}
+
+/// Durability handle associated with the write operation
+///
+/// ## Epoch
+///
+/// Every ticket is assigned a monotonically increasing epoch to moniter durability
+///
+/// ## Durability Guarantee
+///
+/// If wanted, the ticket could be awaited to poll till the epoch becomes durable.
+///
+/// Once a await on ticket is completed successfully, all writes assigned to earlier epochs are
+/// also guaranteed to be durable.
+///
+/// *NOTE:* Using `await` is optional. Callers that only require fire-and-forget semantics may
+/// simply discard the returned ticket.
+#[derive(Debug)]
+pub struct AckTicket {
+    epoch: TEpoch,
+    completion: sync::Arc<Completion>,
+    listener: Option<event_listener::EventListener>,
+}
+
+impl AckTicket {
+    /// Construct a new [`AckTicket`]
+    #[inline]
+    pub const fn new(epoch: TEpoch, completion: sync::Arc<Completion>) -> Self {
+        Self { epoch, completion, listener: None }
+    }
+
+    /// Read assigned durability epoch for the [`AckTicket`]
+    #[inline(always)]
+    pub const fn epoch(&self) -> TEpoch {
+        self.epoch
+    }
+
+    #[inline]
+    fn is_ready(&self) -> bool {
+        self.completion.read_durable_epoch() >= self.epoch
+    }
+}
+
+impl future::Future for AckTicket {
+    type Output = FrozenResult<TEpoch>;
+
+    fn poll(mut self: pin::Pin<&mut Self>, cx: &mut task::Context<'_>) -> task::Poll<Self::Output> {
+        loop {
+            if self.is_ready() {
+                return task::Poll::Ready(Ok(self.epoch));
+            }
+
+            if let Some(frozen_err) = self.completion.get_err() {
+                return task::Poll::Ready(Err(frozen_err));
+            }
+
+            if self.listener.is_none() {
+                self.listener = Some(self.completion.event.listen());
+
+                // NOTE: prevent lost wakeups
+                continue;
+            }
+
+            let listener = self.listener.as_mut().unwrap();
+            match pin::Pin::new(listener).poll(cx) {
+                task::Poll::Ready(()) => {
+                    self.listener = None;
+
+                    // NOTE: Re-check durable epoch & error
+                    continue;
+                }
+
+                task::Poll::Pending => {
+                    return task::Poll::Pending;
+                }
+            }
+        }
     }
 }
